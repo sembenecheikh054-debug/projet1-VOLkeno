a7aa2ec2363377ea62b6ce36fcd1f00c668f04ca
Cheikh <sembenecheikh054@gmail.com>
2026-05-07 17:29:01 +0000

    changement de la page inscription

--- inscription.js
+++ inscription.js
@@ -1,25 +1,46 @@
 async function register() {
-    const nom      = document.getElementById("nom").value;
-    const email    = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirm  = document.getElementById("confirm").checked;
-    const erreur   = document.getElementById("erreur");
 
-    if (!nom || !email || !password || !confirm) {
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirm = document.getElementById("confirm").checked;
+    const erreur = document.getElementById("erreur");
+
+    if (!nom || !email || !password) {
         erreur.textContent = "Remplir tous les champs";
         return;
     }
 
     if (!confirm) {
-        erreur.textContent = "Veuiller accepter les conditions";
+        erreur.textContent = "Veuillez accepter les conditions";
         return;
     }
 
-    const res = await request("/auth/register", "POST", { nom, email, password });
+    const res = await request(
+        "/auth/register",
+        "POST",
+        { nom, email, password }
+    );
+
     console.log(res);
-    if (res.message === "Compte créé ✅") {
-        window.location.href = "both.html";
+
+    // succès inscription
+    if (res.message === "Compte créé. Vérifiez votre email ✅") {
+
+        erreur.classList.remove("text-red-500");
+
+        erreur.classList.add("text-green-600");
+
+        erreur.textContent =
+            "Compte créé. Vérifiez votre email pour activer votre compte.";
+
     } else {
-        erreur.textContent = res.message;
+
+        erreur.classList.remove("text-green-600");
+
+        erreur.classList.add("text-red-500");
+
+        erreur.textContent =
+            res.message || "Erreur";
     }
 }
